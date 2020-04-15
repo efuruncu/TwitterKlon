74ff51d8b45f69ccb75a14f0468361edfe994dfa
efuruncu <eliffuruncu93@gmail.com>
2020-04-15 14:59:58 +0300

    firebase initialization

--- src/App.js
+++ src/App.js
@@ -10,9 +10,22 @@ import React,{Component} from 'react';
 import {  StyleSheet, View,  Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
   export default class App extends Component{
+    componentDidMount(){
+      const firebaseConfig = {
+        apiKey: "AIzaSyCumITmVE-XLN6_QJ9JMOOmxSIyxRJ_4G8",
+        authDomain: "twitterklon-53391.firebaseapp.com",
+        databaseURL: "https://twitterklon-53391.firebaseio.com",
+        projectId: "twitterklon-53391",
+        storageBucket: "twitterklon-53391.appspot.com",
+        messagingSenderId: "188076555183",
+        appId: "1:188076555183:web:58fcbbe746143bef978d5a"
+      };
+      firebase.initializeApp(firebaseConfig);
+    }
     render(){
       return(
         <Provider store={createStore(reducers)}>
